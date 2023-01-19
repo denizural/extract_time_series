02f79f2c04c81fbb486ba2574195bf97fe0f8331
Deniz Ural <denizural86@gmail.com>
2023-01-19 12:04:44 +0100

    adds extract_time_series.py

---
+++ extract_time_series.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""extract_time_series.py: A command-line tool to extract time series from ERA5-Land dataset"""
+
+__author__ = "Deniz Ural"
+__authors__ = ["Deniz Ural"]
+__contact__ = "denizural86@gmail.com"
+__copyright__ = ""
+__date__ = "2023/01/19"
+__email__ =  "denizural86@gmail.com"
+__license__ = "GPLv3"
+__maintainer__ = "Deniz Ural"
+__status__ = "Development"  # Production
+__version__ = "0.0.1"
+
+import argparse
